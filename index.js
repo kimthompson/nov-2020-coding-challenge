03e090d660ef146b60585ccd10bdbcefec1382c1
Kimberly Thompson <kat0911@MSP42KAT110323.local>
2020-11-13 15:47:56 -0600

    diagnostics

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+import chalk from 'chalk'
 import getData from './src/getData'
 
-let data
+const MAX_FLOUR = 60000 // in grams
 
 async function app() {
-  data = await getData('https://raw.githubusercontent.com/emilionavarro/daugherty-nov-challenge/main/pastry_orders.json')
+  const data = await getData('https://raw.githubusercontent.com/emilionavarro/daugherty-nov-challenge/main/pastry_orders.json')
 
-  console.log('data', data)
+  data.forEach((order) => {
+    let price = 0
+    let flourNeeded = 0
+
+    console.log(chalk.magenta(`${order.length} items`))
+    order.forEach((item) => {
+      //console.log(`${item.quantity} ${item.name} @ $${item.price} each`)
+      price += item.price * item.quantity
+      flourNeeded += item.flourConsumption * item.quantity
+    })
+
+    console.log(`price: $${price}`)
+    console.log(`flour needed: ${Math.round(flourNeeded)} grams`)
+  })
 }
 
 app()
